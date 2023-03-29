6faeae56bbc3171c6e172254c958c0e92a9bed4b
victorvadmandjensen <91823836+victorvadmandjensen@users.noreply.github.com>
2023-03-29 18:42:14 +0200

    fix_waiting
    It works??? In testing it seems like our waiting room feature actually works to ensure that if you end a third round and submit without getting too few tokens in the next round. However now the form does not clear.

--- classes/player.py
+++ classes/player.py
@@ -4,16 +4,6 @@ class Player:
         self.tokens = 0
         self.base_tokens = base_tokens
 
-    def enter_tokens_spent(self):
-        to_actions = int(input('Spend tokens on action cards or player actions for the ' + self.role + ' player: ') )
-        #print('I spent ' + to_actions + ' tokens!')
-        return to_actions
-    
-    def enter_tokens_battery(self):
-        to_battery = int(input('Send tokens to battery for the ' + self.role + ' player: ') )
-        #print('I sent ' + to_battery + ' tokens!')
-        return to_battery
-
     def update_tokens(self, tokens_spent, tokens_battery):        
         self.tokens = self.tokens - tokens_spent - tokens_battery
         #print('Total: ' + str(self.tokens))
