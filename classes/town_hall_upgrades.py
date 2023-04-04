e53224a7a27f203c5ad1f7c18206c27f9eb62daa
victorvadmandjensen <91823836+victorvadmandjensen@users.noreply.github.com>
2023-04-04 12:53:52 +0200

    fix_upgrades
    Our upgrades are now fixed! Yay

--- classes/town_hall_upgrades.py
+++ classes/town_hall_upgrades.py
@@ -8,7 +8,7 @@ class Town_Hall_Upgrades:
     
 
 # creation of upgrades - IDs are zero-indexed
-token_upgrade = Town_Hall_Upgrades(0, "Token upgrade", "Every player gets an extra token in every turn for the rest of the game.", 0, 1)
+token_upgrade = Town_Hall_Upgrades(0, "Token upgrade", "Every player gets an extra token in every turn for the rest of the game. This will lead to other citizens in Energy City using 2 more energy units every round.", -2, 1)
 energy_upgrade = Town_Hall_Upgrades(1, "Energy upgrade", "All public institutions and private companies produce 1 more energy each round, for a total of 4 energy units.", 4, 0)
 
 town_hall_upgrades_list = [token_upgrade, energy_upgrade]
