9fc57171659fcf450ed6581b63dcc47d7e50300d
victorvadmandjensen <91823836+victorvadmandjensen@users.noreply.github.com>
2023-04-07 14:49:45 +0200

    Update season.py

--- classes/season.py
+++ classes/season.py
@@ -7,9 +7,9 @@ class Season:
     def getSeasonText(self):
         return self.name
 
-spring = Season("Spring",'It is now spring! Energy production is not affected this round. \n', 0)
-summer = Season("Summer", 'It is now summer! Energy production is increased by 3 energy units. \n', 3)
-fall = Season("Fall",'It is now fall! Energy production is decreased by 3 energy units. \n', -3)
-winter = Season("Winter",'It is now winter! Energy production is decreased by 6 energy units. \n', -6)
+spring = Season("Spring",'It is now spring! The birds have started chirping, and the sun peeks through the clouds. Energy production is not affected this round, as there is not enough sun to really power our solar cells more than usual. \n', 0)
+summer = Season("Summer", 'It is now summer! The flowers are in bloom, and we are really feeling the heat. Energy production is increased by 3 energy units, as our solar cells can capture all the sun\' s rays, and Lake May Hospital\'s water mill spins. \n', 3)
+fall = Season("Fall",'It is now fall! That means it is getting colder, and there is less sun. Energy production is decreased by 3 energy units in this season, as we now have less sun to rely on. \n', -3)
+winter = Season("Winter",'It is now winter! The days have become a lot shorter, and we are starting to huddle up inside. Energy production is decreased by 6 energy units, as our communtiy battery is less effective in cold weather and we have very little sun for our solar cells. \n', -6)
 
 season_list = [spring, summer, fall, winter]
